04090ac66e6d03ba393a4e2ad3fcb1a1dc011025
mouWorks <mou.wang@g.ncu.edu.tw>
2019-11-25 12:01:24 +0800

    go

--- server.js
+++ server.js
@@ -9,8 +9,27 @@ const mysql = require('mysql');
 const cors = require('cors');
 const path = require('path');
 
-console.log('Hey Dcard 0001');
+console.log('Hey Dcard 0002');
 
 let message = 'Dcard Demo @port 3003';
 
 app.get('/', (req, res) => res.send(message));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    // res.sendFile(path.join(__dirname + '/static/html/error.html'));
+    // res.render('error');
+});
+
+app.listen(3003, () => console.log('Listening on 3003 port'));
