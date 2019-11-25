f4d35d0bf4613a80b80090cbb0d4c6bc27e92251
mouWorks <mou.wang@g.ncu.edu.tw>
2019-11-25 11:35:01 +0800

    go

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 "use strict";
 
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const app = express();
+const log = require('morgan');
+const mysql = require('mysql');
+const cors = require('cors');
+const path = require('path');
+
 let message = 'Dcard Demo @port 3003';
 
 app.get('/', (req, res) => res.send(message));
