22823d472ca56c2fd7ad162e3f3db36a7ccc2354
mouWorks <mou.wang@g.ncu.edu.tw>
2019-11-28 12:43:58 +0800

    Flip back

--- modules/async-db.js
+++ modules/async-db.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const mysql = require('mysql');
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: 'circleci',
-    database: 'mouWorks'
+    user: process.env['DB_USER'],
+    password: process.env['DB_PASS'],
+    database: process.env['DB_NAME']
 });
 
 let query = function( sql, values ) {
